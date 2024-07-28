02e06b8701e0678bbee5c873d82790be027d5cc4
abdalkareem mounir <abdalkareem.m.2001@icloud.com>
2024-07-28 18:19:09 +0300

    time function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from function import manage_csv
-
+import time
 
 
 def read_data():
     x = input("enter meters: ")
     y = input("enter price: ")
     z = input("enter type: ")
-    return [x,y,z]
+    t = time.strftime("%d-%m-%Y")
+    return [x,y,z,t]
 
 """df1 = pandas.DataFrame([[5,100,"blosh"],[10,100,"blosh"]],columns=["meters","price","type"])
 """
