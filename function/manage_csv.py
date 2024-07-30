6fb9236928f4f7a0dbf9ebf92c962d1daae6ab9c
abdalkareem mounir <abdalkareem.m.2001@icloud.com>
2024-07-30 12:00:04 +0300

    implement show docementation

--- function/manage_csv.py
+++ function/manage_csv.py
@@ -7,3 +7,10 @@ def add_toCSV(df_local,path):
         df1_T[len(df1)]=df_local
         df1 = df1_T.T
         df1.to_csv('assi_shop.csv', index=False) 
+        
+def show_csv(path):
+    if os.path.exists(path):
+        df1 = pandas.read_csv(path)
+        return df1
+    else:
+        return None
--- web.py
+++ web.py
@@ -1,25 +1,33 @@
 from function import manage_csv
 import streamlit as st
 import time
+from streamlit_navigation_bar import st_navbar
 
 
-    
-with st.form("my_form"):
-    st.title("Assi shop app")
+page = st_navbar(["Home", "Documentation","Dashbord"])
+st.write(page)
+if page == "Home":
+    with st.form("my_form"):
+        st.title("Assi shop app")
 
-    st.text_input(label="",placeholder="Add meter..",
-                key="meter")
-    st.text_input(label="",placeholder="Add price..",
-                key="price")
-    st.text_input(label="",placeholder="Add type..",
-                key="type")
+        st.text_input(label="",placeholder="Add meter..",
+                    key="meter")
+        st.text_input(label="",placeholder="Add price..",
+                    key="price")
+        st.text_input(label="",placeholder="Add type..",
+                    key="type")
 
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        meter = st.session_state["meter"]
-        price = st.session_state["price"]
-        type_sold = st.session_state["type"]
-        time_sold = time.strftime("%d-%m-%Y")
-        mylist = [meter,price,type_sold,time_sold]
-        manage_csv.add_toCSV(mylist,"assi_shop.csv")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            meter = st.session_state["meter"]
+            price = st.session_state["price"]
+            type_sold = st.session_state["type"]
+            time_sold = time.strftime("%d-%m-%Y")
+            mylist = [meter,price,type_sold,time_sold]
+            manage_csv.add_toCSV(mylist,"assi_shop.csv")
+            st.write("meter",meter,"price",price,"type",type_sold,"time",time_sold)
 
+elif page == "Documentation":
+    st.write("data")
+    df1 = manage_csv.show_csv("assi_shop.csv")
+    st.dataframe(df1)
